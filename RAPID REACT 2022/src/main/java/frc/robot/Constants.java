a0c01fad973674e7eeb6f903f196068052e69c2f
Blockinator1 <70295628+Blockinator1@users.noreply.github.com>
2022-02-05 11:05:20 -0700

    Squashed commit of the following:
    commit 262bbf1c7253b72997598e58cc6d333bc0b521f2
    Author: Blockinator1 <70295628+Blockinator1@users.noreply.github.com>
    Date:   Sat Feb 5 11:01:38 2022 -0700
    
        Add JoystickValues to Constants.java
    
        Added Joystick button and axis values to Constants, along with an empty deadzone value and Motors class for motor id's.

--- RAPID REACT 2022/src/main/java/frc/robot/Constants.java
+++ RAPID REACT 2022/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +14,27 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class Motors {
+        
+    }
+    public static final class JoystickValues {
+        public static final int A = 2;
+        public static final int B = 3;
+        public static final int X = 1;
+        public static final int Y = 4;
+        public static final int Start = 10;
+        public static final int Back = 9;
+        public static final int LeftBumper = 5;
+        public static final int RightBumper = 6;
+        public static final int LeftTrigger = 7;
+        public static final int RightTrigger = 8;
+        public static final int DpadXaxis = 5;
+        public static final int DpadYaxis = 6;
+        public static final int LeftJoystickX = 2;
+        public static final int LeftJoystickY = 3;
+        public static final int RightJoystickX = 3;
+        public static final int RightJoystickY = 4;
+    }
+    public static final float deadzone = 0;
+}
