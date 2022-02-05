433c734a6d197d3bb2ba8bc27e655fa778522597
Blockinator1 <70295628+Blockinator1@users.noreply.github.com>
2022-02-05 11:17:04 -0700

    Fixed LeftJoystickY
    LeftJoystickY value was wrong, fixed it.

--- RAPID REACT 2022/src/main/java/frc/robot/Constants.java
+++ RAPID REACT 2022/src/main/java/frc/robot/Constants.java
@@ -33,7 +33,7 @@ public final class Constants {
         public static final int DpadXaxis = 5;
         public static final int DpadYaxis = 6;
         public static final int LeftJoystickX = 2;
-        public static final int LeftJoystickY = 3;
+        public static final int LeftJoystickY = 1;
         public static final int RightJoystickX = 3;
         public static final int RightJoystickY = 4;
     }
