a529c79d15803348a4564a5c3ce3ec0cc0557648
small-furry-creature-of-Alpha-Centauri <83842439+critter-ACentauri@users.noreply.github.com>
2022-02-08 17:10:47 -0700

    Added JoyUtil class
    Added a Joystick Utility class

---
+++ src/main/java/frc/robot/JoyUtil.java
@@ -0,0 +1,8 @@
+public class JoyUtil {
+
+    /* ++ we'll use this class to write methods that help us process joystick inputs
+    * and will mostly be for drive train things, and will include things like:
+    * deadzone functions and joystick input curves, and anything else we need. 
+    */
+    
+}
