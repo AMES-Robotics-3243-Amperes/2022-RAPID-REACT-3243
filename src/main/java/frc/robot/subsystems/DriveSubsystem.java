505a54002e7bd74ae82a7df6a58c259cba6faee3
MikeVey42 <steelege07@gmail.com>
2022-02-23 21:05:20 -0700

    Autonomous PIDs

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,21 +4,32 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
-import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
+
 import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
+import com.kauailabs.navx.frc.AHRS;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import frc.robot.Constants;
 
 
@@ -43,10 +54,13 @@ public class DriveSubsystem extends SubsystemBase {
   private final SparkMaxPIDController backRightPIDController;
 
   // ++ Gyro and Acceleromter
-  
+  AHRS imu = new AHRS();
 
   // ++ Field Object for visulaization in shuffleboard or simulation
   Field2d field = new Field2d();
+
+  // ++ Pose object for keeping track of robot position
+  Pose2d pose = new Pose2d(6.0, 4.0, new Rotation2d());
 
   // ++ mecanum drive kinematics object
   MecanumDriveKinematics kinematics = new MecanumDriveKinematics(
@@ -94,10 +108,70 @@ public class DriveSubsystem extends SubsystemBase {
     iGainWidget = pidTab.add("I gain", 0.0);
     dGainWidget = pidTab.add("D gain", 0.0);
 
+    resetGyroRotation();
+
+    resetPose();
+
   }
 
-  public void driveCartesian (double X_speed, double Y_speed, double Z_rotation) {
-    // speeds.driveCartesian(-Y_speed, X_speed, Z_rotation);
+  // ++ returns a Rotation2d object with the robot's current angle, in radians
+  public Rotation2d getGyroRotation() {
+    Rotation2d rotation = new Rotation2d(-imu.getYaw() * Math.PI / 180);
+    return rotation;
+  }
+
+  public void resetGyroRotation() {
+    imu.zeroYaw();
+  }
+
+  // ++ resets the Pose2d and encoder positions of all the motors
+  public void resetPose() {
+    Pose2d pose = new Pose2d(6.0, 4.0, new Rotation2d());
+    ChassisSpeeds chassisPos = new ChassisSpeeds(pose.getX(), pose.getY(), pose.getRotation().getRadians());
+    MecanumDriveWheelSpeeds wheelPos = kinematics.toWheelSpeeds(chassisPos);
+    frontLeftEncoder.setPosition(wheelPos.frontLeftMetersPerSecond);
+    frontRightEncoder.setPosition(wheelPos.frontRightMetersPerSecond);
+    backLeftEncoder.setPosition(wheelPos.rearLeftMetersPerSecond);
+    backRightEncoder.setPosition(wheelPos.rearRightMetersPerSecond);
+
+    // IMPORTANT - we may need to set motor positional PID loop to the encoder position after resetting to prevent runaway robots.
+    // Implementation would be:
+    // frontLeftPIDController.setReference(frontLeftEncoder.getPosition(), ControlType.kPosition);
+    // frontRightPIDController.setReference(frontRightEncoder.getPosition(), ControlType.kPosition);
+    // backLeftPIDController.setReference(backLeftEncoder.getPosition(), ControlType.kPosition);
+    // backRightPIDController.setReference(backRightEncoder.getPosition(), ControlType.kPosition);
+
+    imu.resetDisplacement();
+  }
+
+  public void changeRobotPosition(Pose2d transform) {
+    ChassisSpeeds chassisTransform = new ChassisSpeeds(transform.getX(), transform.getY(), transform.getRotation().getRadians());
+    MecanumDriveWheelSpeeds wheelTransform = kinematics.toWheelSpeeds(chassisTransform);
+
+    double frontLeftPos = frontLeftEncoder.getPosition() + wheelTransform.frontLeftMetersPerSecond;
+    double frontRightPos = frontRightEncoder.getPosition() + wheelTransform.frontRightMetersPerSecond;
+    double backLeftPos = backLeftEncoder.getPosition() + wheelTransform.rearLeftMetersPerSecond;
+    double backRightPos = backRightEncoder.getPosition() + wheelTransform.rearRightMetersPerSecond;
+
+    frontLeftPIDController.setReference(frontLeftPos, ControlType.kPosition);
+    frontRightPIDController.setReference(frontRightPos, ControlType.kPosition);
+    backLeftPIDController.setReference(backLeftPos, ControlType.kPosition);
+    backRightPIDController.setReference(backRightPos, ControlType.kPosition);
+  }
+
+  public void setRobotPosition(Pose2d position) {
+    ChassisSpeeds chassisPos = new ChassisSpeeds(position.getX(), position.getY(), position.getRotation().getRadians());
+    MecanumDriveWheelSpeeds wheelPos = kinematics.toWheelSpeeds(chassisPos);
+
+    double frontLeftPos = wheelPos.frontLeftMetersPerSecond;
+    double frontRightPos = wheelPos.frontRightMetersPerSecond;
+    double backLeftPos = wheelPos.rearLeftMetersPerSecond;
+    double backRightPos = wheelPos.rearRightMetersPerSecond;
+
+    frontLeftPIDController.setReference(frontLeftPos, ControlType.kPosition);
+    frontRightPIDController.setReference(frontRightPos, ControlType.kPosition);
+    backLeftPIDController.setReference(backLeftPos, ControlType.kPosition);
+    backRightPIDController.setReference(backRightPos, ControlType.kPosition);
   }
 
   public void getShuffleboardPID() {
@@ -147,9 +221,17 @@ public class DriveSubsystem extends SubsystemBase {
     backRightPIDController.setIAccum(0);
   }
 
+  public void driveCartesian (double X_speed, double Y_speed, double Z_rotation) {
+    // speeds.driveCartesian(-Y_speed, X_speed, Z_rotation);
+  }
+
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    double xPos = 6.0 + imu.getDisplacementX();
+    double yPos = 4.0 + imu.getDisplacementY();
+    pose = new Pose2d(xPos, yPos, getGyroRotation());
+
+    field.setRobotPose(pose);
   }
 
   @Override
