e1f97c1864fdf2894d4897f678b0b21ab0aad625
small-furry-creature-of-Alpha-Centauri <83842439+critter-ACentauri@users.noreply.github.com>
2022-03-17 17:20:28 -0600

    changed some constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -83,10 +83,10 @@ public final class Constants {
         // ++ OTHER JOYSTICK CONSTANTS --------------------------------------------------------------
         public static final double deadZoneSize = 0.15;
         // ++ lowPassFilterStrength should be between 0 & 1. The closer it is to 1, the smoother it is. 
-        public static final double driveLowPassFilterStrength = 0.93;
+        public static final double driveLowPassFilterStrength = 0.91;
         public static final double rotationLowPassFilterStrength = 0.2;
         // ++ we probably don't want the speed dampers as finals incase we want a fastmode/to change them later
-        public static final double driveSpeedDamper = 0.5; 
+        public static final double driveSpeedDamper = 0.4; 
         public static final double rotationDamper = 2.0; 
 
 
