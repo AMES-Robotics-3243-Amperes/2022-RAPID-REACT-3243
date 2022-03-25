9719b8b30cb303d42cf242d9cc252e81b21ae9a8
MikeVey42 <steelege07@gmail.com>
2022-03-25 14:04:44 -0600

    Added JoyCurveOption description

--- src/main/java/frc/robot/JoyCurveOption.java
+++ src/main/java/frc/robot/JoyCurveOption.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+/**
+ * The JoyCurveOption Class contains all the values for a joystick curve function.
+ * In combination with a SendableChooser, this allows us to create several presets for joystick curves
+ * which drivers can switch between depending on their preference
+ */
 public class JoyCurveOption {
     private final int firstPower, secondPower;
     private final double aCoeff;
