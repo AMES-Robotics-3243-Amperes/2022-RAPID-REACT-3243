47b353fba37a548f245d9681b41efba2e978dde8
small-furry-creature-of-Alpha-Centauri <83842439+critter-ACentauri@users.noreply.github.com>
2022-03-26 18:58:36 -0600

    changed RebuffCommand to match AcceptCommand; made it not need any paramaters (other than subsystem)

--- src/main/java/frc/robot/commands/IntakeIndexer/AcceptCommand.java
+++ src/main/java/frc/robot/commands/IntakeIndexer/AcceptCommand.java
@@ -50,6 +50,6 @@ public class AcceptCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (timer.get() > Constants.IntakeIndexer.acceptDuration);
+    return (timer.get() >= Constants.IntakeIndexer.acceptDuration);
   }
 }
--- src/main/java/frc/robot/commands/IntakeIndexer/RebuffCommand.java
+++ src/main/java/frc/robot/commands/IntakeIndexer/RebuffCommand.java
@@ -6,22 +6,17 @@ package frc.robot.commands.IntakeIndexer;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.IntakeIndexerSubsystem;
 
 public class RebuffCommand extends CommandBase {
 
   private final IntakeIndexerSubsystem m_subsystem;
-  private final double m_rotations;
-  private final double m_intakeSpeed;
-  private final double m_duration;
   private final Timer timer;
 
   /** Creates a new RebuffCommand. */
-  public RebuffCommand(IntakeIndexerSubsystem subsystem, double rotations, double intakeSpeed, double duration) {
+  public RebuffCommand(IntakeIndexerSubsystem subsystem) {
     m_subsystem = subsystem;
-    m_rotations = rotations;
-    m_intakeSpeed = intakeSpeed;
-    m_duration = duration;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_subsystem);
 
@@ -33,24 +28,25 @@ public class RebuffCommand extends CommandBase {
   public void initialize() {
     timer.reset();
     timer.start();
-    m_subsystem.stepIndexer(m_rotations);
+    m_subsystem.setIntakeSpeed(Constants.IntakeIndexer.rebuffSpeed);
+    m_subsystem.stepIndexer(Constants.IntakeIndexer.rebuffRotations);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_subsystem.setIntakeSpeed(m_intakeSpeed);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_subsystem.setIntakeSpeed(0.0);
+    m_subsystem.stepIndexer(0.0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (timer.get() > m_duration);
+    return (timer.get() >= Constants.IntakeIndexer.rebuffDuration);
   }
 }
